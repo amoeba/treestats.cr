f7a3c9e7e41f732fe9216af2dc8a97d8044c5147
Bryce Mecum <petridish@gmail.com>
2018-08-25 11:02:03 -0800

    Update mongo.cr and fix app back up

--- src/treestats.cr
+++ src/treestats.cr
@@ -2,9 +2,8 @@ require "kemal"
 require "mongo"
 
 client = Mongo::Client.new "mongodb://127.0.0.1:27017/test"
-db = client["test"]
+db = client["treestats-test"]
 
-populations = db["populations"]
 characters = db["characters"]
 
 module Treestats
@@ -12,12 +11,10 @@ module Treestats
 end
 
 post "/character" do |env|
-  characters.insert({ "server" => "test2", "name" => "test"})
+  # TODO: Implement create/update feature
+  characters.insert({ "server" => "Test", "name" => "Test"})
 end
 
-post "/population" do |env|
-  populations.insert({ "server" => "test2", "count" => 100})
-end
 
 get "/" do
   render "src/views/index.ecr"
@@ -25,19 +22,29 @@ end
 
 get "/characters" do
   chars = characters.find({} of String => String)
+
   render "src/views/characters.ecr"
 end
 
-get "/character/:server/:character" do |env|
-  server = env.params.url["server"]
-  character = env.params.url["character"]
+get "/character/:server/:name" do |env|
+  character = characters.find({
+    "server" => env.params.url["server"],
+    "name" => env.params.url["name"]
+  })
 
   render "src/views/character.ecr"
 end
 
 get "/servers" do
-  pipeline = [ { "$group" => { "_id" => "$server", "nchars" => { "$sum" => 1 }} } ]
-  servs = characters.aggregate(pipeline)
+  pipeline = [{
+    "$group" => { 
+      "_id" => "$server", 
+      "nchars" => { "$sum" => 1 }
+    }
+  }]
+
+  servers = characters.aggregate(pipeline)
+
   render "src/views/servers.ecr"
 end
 
@@ -45,12 +52,6 @@ get "/server/:server" do |env|
   server = env.params.url["server"]
 
   render "src/views/server.ecr"
-end
-
-get "/populations" do
-  pops = populations.find({} of String => Int32)
-
-  render "src/views/populations.ecr"
 end
 
 Kemal.run
